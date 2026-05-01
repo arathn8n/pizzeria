6154ffff153d66f019a1d2eff7104519a3be6ff8
arath abello <arathabello60@gmail.com>
2026-05-01 11:09:50 -0400

    api terminada

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,40 +3,45 @@ import './card.css'
 
 const Card = (props) => {
   const [contador, setContador] = useState(0)
-  
-  function handleClickIncrement(){
+
+  function handleClickIncrement() {
     setContador(contador + 1)
   }
 
-  function handleClickDecrement(){
-    if(contador > 0){
+  function handleClickDecrement() {
+    if (contador > 0) {
       setContador(contador - 1)
     }
   }
-  
+
 
   return (
     <>
-  <div className="card col-12 col-md-4" >
-    <img src={props.img} className="card-img-top" alt={props.name}/>
-    <div className="card-body">
-      <h5 className="card-title">Pizza {props.name}</h5>
-      <p className="card-text">🍕 Ingredientes: <br />
-        {props.ingredients.join(', ')}
-      </p>
-      <p>precio: {props.price}</p>
-      <button type="button" className="btn btn-light">Ver más</button>
-      <div className='d-flex'>
-        <button type="button" className="btn btn-primary" onClick={handleClickIncrement}>Añadir</button>
-        <p className='mx-2'>{contador}</p>
-        <button type="button" className="btn btn-danger" onClick={handleClickDecrement}>quitar</button>
+      <div className="card col-12 col-md-4" >
+        <img src={props.img} className="card-img-top" alt={props.name} />
+        <div className="card-body">
+          <h5 className="card-title">Pizza {props.name}</h5>
+          <ul>
+  🍕 Ingredientes:
+  {props.ingredientes?.map((ingrediente, id) => (
+    <li key={id}>{ingrediente}</li>
+  ))}
+</ul>
+
+
+          <p>precio: {props.price}</p>
+          <button type="button" className="btn btn-light">Ver más</button>
+          <div className='d-flex'>
+            <button type="button" className="btn btn-primary" onClick={handleClickIncrement}>Añadir</button>
+            <p className='mx-2'>{contador}</p>
+            <button type="button" className="btn btn-danger" onClick={handleClickDecrement}>quitar</button>
+          </div>
+        </div>
+
       </div>
-    </div>
 
-  </div>
+    </>
 
-  </>
-  
   )
 }
 
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,25 @@
 import './home.css'
 import Card from '../card/Card'
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {pizzas} from '../data/pizzas'
 
 const Home = () => {
   
-  const [datos, setDatos] = useState(pizzas)
+  // const [datos, setDatos] = useState(pizzas)
+  const [apiPizzas, setApiPizzas] = useState({})
+
+  useEffect(()=>{
+    pizzasApi()
+
+  }, [])
+
+  async function pizzasApi() {
+    const url = 'http://localhost:5000/api/pizzas/p001'
+    const response = await fetch(url)
+    const data = await response.json()
+
+    setApiPizzas(data)
+  }
 
   return (
     <>
@@ -20,39 +34,18 @@ const Home = () => {
 
     <section className='container'>
       <div className='row'>
-        
-        {/* <Card name="Napolitana"
-          price={5950}
-          ingredients={["mozzarella", "tomates", "jamón", "orégano"]}
-          img="https://tse2.mm.bing.net/th/id/OIP.YxBwJv6mziFtNgIbye3EhwHaE8?rs=1&pid=ImgDetMain&o=7&rm=3" />
-        
-        
-        
-        <Card name="Española"
-          price={6950}
-          ingredients={["mozzarella", "gorgonzola", "parmesano", "provolone"]}
-          img="https://tse4.mm.bing.net/th/id/OIP.Z3-PsMls1jZnfb6n8-FFmgHaE8?rs=1&pid=ImgDetMain&o=7&rm=3" />
-        
 
-        
-          <Card name="Pepperoni"
-            price={6950}
-            ingredients={["mozzarella", "pepperoni", "orégano"]}
-            img="https://th.bing.com/th/id/OIP.K4rzg3rTYg4Hk6ZBX_Fs9gHaE8?o=7rm=3&rs=1&pid=ImgDetMain&o=7&rm=3"/> */}
-        
-
-        
-        
-          {datos.map(data => 
-            <Card key={data.id}
-              img={data.img}
-              name={data.name}
-              price={data.precio}
-              ingredients={data.ingredients}
-              descripcion={data.desc} 
+          
+            <Card key={apiPizzas.id}
+              img={apiPizzas.img}
+              name={apiPizzas.name}
+              price={apiPizzas.price}
+              ingredientes={apiPizzas.ingredients}
+              descripcion={apiPizzas.desc} 
             />
-          )}
+          
 
+         
           
       </div>
     </section>
