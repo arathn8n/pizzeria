5fc7191a4980c0e12b38de32a3a53c5e7e1d11ef
arath abello <arathabello60@gmail.com>
2026-05-05 17:04:56 -0400

    comenzando jwt error 404

--- src/components/formulario/Form.jsx
+++ src/components/formulario/Form.jsx
@@ -2,34 +2,54 @@ import React, { useState } from 'react'
 import './form.css'
 const Form = () => {
 
-   const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
-  const validarInput = (e)=>{
+  function handleEmail(e) {
+    setEmail(e.target.value)
+  }
+
+  function handlePassword(e) {
+    setPassword(e.target.value)
+  }
+
+  const validarInput = async (e) => {
     e.preventDefault()
 
-    if(password.length < 8){
+    if (password.length < 8) {
       alert('la contraseña debe ser de al menos 8 digitos')
-    }else{
+    } else {
       alert('logueado correctamente!')
     }
+
+   const url = await fetch("http://localhost:5000/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer token_jwt`,
+      },
+      body: JSON.stringify({email, password})
+    });
+    const data = await url.json()
+    console.log(data)
   }
 
   return (
     <div className='contentForm'>
-    <form onSubmit={validarInput}>
-  <div className="mb-3">
-    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div className="mb-3">
-    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1" onChange={(e) => setPassword(e.target.value)} />
-  </div>
-  
-  <button type="submit" className="btn btn-primary">Login</button>
-</form>
-</div>
+      <form onSubmit={validarInput}>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={handleEmail} value={email} />
+          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+          <input type="password" className="form-control" id="exampleInputPassword1" onChange={handlePassword} value={password} />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Login</button>
+      </form>
+    </div>
   )
 }
 
